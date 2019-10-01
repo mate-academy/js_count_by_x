3d00090a4e5c4e2b49bc9dabdc397254d8d6914e
Viktor Lukovets <nuerio63@gmail.com>
2019-10-01 11:58:41 +0300

    Solution

--- src/countByX.js
+++ src/countByX.js
@@ -17,7 +17,12 @@
  * @return {number[]}
  */
 function countByX(x, length) {
-  // write code here
+  const arr = [];
+  for (let i = 1; i <= length; i++) {
+    arr.push(x * i);
+  }
+
+  return arr;
 }
 
 module.exports = countByX;
